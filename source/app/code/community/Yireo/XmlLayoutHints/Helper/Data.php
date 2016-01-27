15eb9922d625891e8a7136f342db143fafb4b254
Jisse Reitsma <info@yireo.com>
2016-01-27 10:39:30 +0100

    Initial commit

---
+++ source/app/code/community/Yireo/XmlLayoutHints/Helper/Data.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Yireo XmlLayoutHints for Magento 
+ *
+ * @author Yireo
+ * @copyright Copyright 2015
+ * @license Open Source License
+ * @link http://www.yireo.com
+ */
+
+/**
+ * XmlLayoutHints helper
+ */
+class Yireo_XmlLayoutHints_Helper_Data extends Mage_Core_Helper_Data
+{
+}
---
+++ source/app/code/community/Yireo/XmlLayoutHints/Model/Observer.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Yireo XmlLayoutHints for Magento
+ *
+ * @author Yireo
+ * @copyright Copyright 2015
+ * @license Open Source License
+ * @link http://www.yireo.com
+ */
+
+/*
+ * Observer
+ */
+class Yireo_XmlLayoutHints_Model_Observer
+{
+    public function coreBlockAbstractToHtmlAfter($observer)
+    {
+        if (Mage::app()->getStore()->isAdmin()) {
+            return $this;
+        }
+
+        $block = $observer->getEvent()->getBlock();
+        $transport = $observer->getTransport();
+        $layoutName = $block->getNameInLayout();
+
+        $originalHtml = $transport->getHtml();
+        $originalHtml = $this->getHtml($layoutName, $originalHtml);
+
+        $transport->setHtml($originalHtml);
+
+        return $this;
+    }
+
+    protected function getHtml($title, $content)
+    {
+        $firstStyle = 'position:relative; border:1px dotted blue; margin:6px 2px; font-size:70%; padding:18px 2px 2px 2px; zoom:1;';
+        $secondStyle = 'position:absolute; left:0; top:0; padding:2px 5px; background:blue; color:white; z-index:998;';
+
+        $html = array();
+        $html[] = '<div style="'.$firstStyle.'">';
+        $html[] = '<div style="'.$secondStyle.'" onmouseover="this.style.zIndex=\'999\'" onmouseout="this.style.zIndex=\'998\'">';
+        $html[] = $title;
+        $html[] = '</div>';
+        $html[] = $content;
+        $html[] = '</div>';
+
+        return implode('', $html);
+    }
+}
